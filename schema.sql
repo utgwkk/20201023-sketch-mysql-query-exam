9aa5871cef9962a543c0f9df2cce1eb3b76f4fd4
utgwkk <utagawakiki@gmail.com>
2020-10-23 19:11:52 +0900

    schema

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `tbl_without_index` (
+    `id` BIGINT UNSIGNED NOT NULL,
+    `done` BOOLEAN NOT NULL DEFAULT FALSE,
+    
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `tbl_with_index` LIKE `tbl`;
+
+CREATE INDEX `done_and_id` ON `tbl_with_index` (`done`, `id`);
